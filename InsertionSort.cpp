98d6ea43f71d4ef6e9abc4d2b68bb68c7382e780
restuchan <restu.chan188@gmail.com>
2025-03-20 13:45:35 +0700

    memasukkan nilai data n

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -21,6 +21,8 @@ void input (){
     cout << "Masukkan Element Array" << endl;
     cout << "======================" << endl;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++){
+        cout << "Data ke-" << (i + 1) << " : "
+    }
 
 }
